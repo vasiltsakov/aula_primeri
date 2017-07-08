0e76f31e4efbf5188050c4165eec90d12e32de1f
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-08 09:36:02 +0300

    Модул 2 Задача - размяна на стойности на a и b

--- Modul2/Modul2/Program.cs
+++ Modul2/Modul2/Program.cs
@@ -11,10 +11,28 @@ namespace Modul2
         static void Main(string[] args)
         {
             //Дефиниране на променливи
+            int a = 0, b = 0, temp = 0;
 
             //Събиране на числа
+            a = 55;
+            b = 10;
+            Console.WriteLine("Стойностите на a и b са:");
+            Console.Write("a = ");
+            Console.WriteLine(a);
+            Console.Write("b = ");
+            Console.WriteLine(b+"\n\n");
+
+
+            temp = a;
+            a = b;
+            b = temp;
 
             //Писане в конзола
+            Console.WriteLine("Новите стойности на a и b са:");
+            Console.Write("a = ");
+            Console.WriteLine(a);
+            Console.Write("b = ");
+            Console.WriteLine(b);
         }
     }
 }
