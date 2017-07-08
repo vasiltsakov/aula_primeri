9d839b9795477e66b2cd35d81f43b2286ec31d14
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-08 09:18:54 +0300

    Модул 2 Добавяне на коментари

---
+++ Modul2/Modul2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дефиниране на променливи
+
+            //Събиране на числа
+
+            //Писане в конзола
+        }
+    }
+}
