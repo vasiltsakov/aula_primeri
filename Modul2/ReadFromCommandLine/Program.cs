bb96dd65994764d6380ad6715c1e31789d779e12
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-08 17:47:40 +0300

    Модул 2 - четене от ред

---
+++ Modul2/ReadFromCommandLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дефиниране на променливи
+            int a = 0, b = 0;
+
+            //Въвеждане на входни параметри
+            Console.Write("Моля въведете стойност за a: ");
+            a = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Моля въведете стойност за b: ");
+            b = Convert.ToInt32( Console.ReadLine());
+
+            //Печат на резултата
+            Console.WriteLine("Сбора от числата е: " + (a + b).ToString());
+            Console.ReadKey();
+        }
+    }
+}
