e6cf3c061ae883f077734a0737464222ed9f2cf0
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-11 21:04:29 +0300

    Модул 3 Математически операции

--- Modul3/Modul3/Program.cs
+++ Modul3/Modul3/Program.cs
@@ -10,13 +10,25 @@ namespace Modul3
     {
         static void Main(string[] args)
         {
-            double a = 0, b = 0;
+            double a = 0, b = 0, c = 0;
             string _format = "#0.00 USD";
 
             Console.Write("> а = "); a = Convert.ToDouble(Console.ReadLine());
             Console.Write("> b = "); b = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("> a / b = " + (a/b).ToString(_format));
+
+            //Степенуване
+            c = Math.Pow(a, b);
+            Console.WriteLine("\n\nСтепенуване a на степен b = " + c.ToString());
+
+            //Коренуване
+            c = Math.Sqrt(b);
+            Console.WriteLine("\n\nКорен квадратен на b = " + c.ToString());
+
+            //Закръгление
+            c = Math.Round(a, 2);
+            Console.WriteLine("\n\nЗакръгление на а = " + c.ToString());
         }
     }
 }
