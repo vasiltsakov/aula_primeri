c94affeb9640503b517ed59ad1810238d9a54bf0
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-11 20:39:26 +0300

    Модул 3 Дробни числа

---
+++ Modul3/Modul3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 0;
+            string _format = "#0.00 USD";
+
+            Console.Write("> а = "); a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("> b = "); b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("> a / b = " + (a/b).ToString(_format));
+        }
+    }
+}
