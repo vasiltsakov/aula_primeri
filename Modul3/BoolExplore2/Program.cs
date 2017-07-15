13b34c1bb76e5a0e82edd17cbc62266bd1c86686
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-15 09:34:10 +0300

    Модул 3 Улавяне на грешки с try catch

--- Modul3/BoolExplore2/Program.cs
+++ Modul3/BoolExplore2/Program.cs
@@ -22,33 +22,43 @@ namespace BoolExplore2
             //_ui = (_ui <= _i.Length) ? _ui : _i.Length;
             //_ui = (_ui > 0) ? _ui : 1;
 
-            switch(_ui)
+            try
             {
-                case 1:
-                    {
-                        Console.Write("Първият елемент на масива е: ");
-                        Console.WriteLine(_i[_ui-1]);
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.Write("Вторият елемент на масива е: ");
-                        Console.WriteLine(_i[_ui - 1]);
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.Write("Третият елемент на масива е: ");
-                        Console.WriteLine(_i[_ui - 1]);
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("В масива няма стойност за този индекс.\nОпитайте с число от 1 до 3.");
-                        break;
-                    }
-
+                Console.Write("Елемент " + _ui.ToString() + " от масива е: ");
+                Console.WriteLine(_i[_ui-1]);
             }
+            catch
+            {
+                Console.WriteLine("недифиниран!");
+            }
+
+            //switch(_ui)
+            //{
+            //    case 1:
+            //        {
+            //            Console.Write("Първият елемент на масива е: ");
+            //            Console.WriteLine(_i[_ui-1]);
+            //            break;
+            //        }
+            //    case 2:
+            //        {
+            //            Console.Write("Вторият елемент на масива е: ");
+            //            Console.WriteLine(_i[_ui - 1]);
+            //            break;
+            //        }
+            //    case 3:
+            //        {
+            //            Console.Write("Третият елемент на масива е: ");
+            //            Console.WriteLine(_i[_ui - 1]);
+            //            break;
+            //        }
+            //    default:
+            //        {
+            //            Console.WriteLine("В масива няма стойност за този индекс.\nОпитайте с число от 1 до 3.");
+            //            break;
+            //        }
+
+            //}
 
             //if (_check_01 )
             //{
