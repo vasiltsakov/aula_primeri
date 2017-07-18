427695b4ecbbab3768d20c77f54ee598828c7e3b
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-18 17:37:02 +0300

    Модул 4 Цикъл с FOR

---
+++ Modul4/Modul4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0;i<=5;i += 2)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+        }
+    }
+}
