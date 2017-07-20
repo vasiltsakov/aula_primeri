021006a006d8e43f5e870631ca9cab4e03a79870
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-20 10:45:35 +0300

    Модул 4 Цикъл с While и Do While

--- Modul4/Modul4/Program.cs
+++ Modul4/Modul4/Program.cs
@@ -10,17 +10,63 @@ namespace Modul4
     {
         static void Main(string[] args)
         {
-            string[] _table = {"Иван","Гошо","Гергана" };
+            string[,] _table = {
+                {"Иван",    "Георгиев", "25" },
+                {"Васил",   "Василев",  "42" },
+                {"Георги",  "Николов",  "54" },
+                {"Валентин","Димов",    "58" },
+                {"Димитър", "Недев",    "36" }
+
+        };
 
             //Антетка на таблица
-            Console.WriteLine("Име");
-            Console.WriteLine("=============");
+            Console.WriteLine("Име".PadRight(12) + "Фамилия".PadRight(12) + "Години".PadRight(12));
+            Console.WriteLine("==============================");
 
             //Тяло на таблица
-            for (int i=0;i<_table.Length;i++)
-            {
-                Console.WriteLine(_table[i]);
-            }
+            //Пример с FOR
+            //for (int i = 0; i < _table.Length/3; i++)
+            //{
+            //    for(int j = 0; j < 3;j++)
+            //    {
+            //        Console.Write(_table[i,j].PadRight(12));
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Пример с While
+            //Console.WriteLine("Дължина на масива: " + _table.Length);
+            //int i = 0;
+            //try
+            //{
+            //    while (i < 6)
+            //    {
+            //        Console.WriteLine(
+            //            _table[i, 0].PadRight(12) +
+            //            _table[i, 1].PadRight(12) +
+            //            _table[i, 2].PadRight(12)
+            //            );
+
+            //        i++;
+            //    }
+            //}
+            //catch(Exception e)
+            //{
+            //    Console.WriteLine("\n\nГрешчицааа!\n\n");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //Пример с Do While
+            int i = 0;
+            do {
+
+                Console.WriteLine(
+                            _table[i, 0].PadRight(12) +
+                            _table[i, 1].PadRight(12) +
+                            _table[i, 2].PadRight(12)
+                            );
+                i++;
+            } while (i < _table.Length/3);
 
         }
     }
---
+++ Modul4/Tasks/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            int i = 0, j = 0;
+            int r_op = 0; // Тук записваме произволната команда
+            int r_num1 = 0; // Номер на първо произволно число от масива
+            int r_num2 = 0; // Номер на второ произволно число от масива
+            var r = new Random();
+
+            string rs = ""; // Тук ще пазим текстовата формула до момента
+            int res = 0; // Тук ще смятаме произволно
+            int found = 0; // Тук пазим колко формули сме намерили до момента
+
+            for (i = 0; i < 100; i++)
+            {
+                rs = ""; // Всяка нова итерация ще изтриваме rs и res 
+                res = 0;
+                for (j = 0; j < 20; j++)
+                {
+                    r_op = r.Next(0, 3); // Тук се решава дали ще събираме, изваждаме или делим
+                    r_num1 = r.Next(0, 3);
+                    r_num2 = r.Next(0, 3);
+
+                    switch (r_op)
+                    {
+                        case 0:
+                            {
+                                res += a[r_num1] + a[r_num2];
+                                rs += a[r_num1] + " + " + a[r_num2] + " + ";
+                                break;
+                            }
+                        case 1:
+                            {
+                                res += a[r_num1] * a[r_num2];
+                                rs += a[r_num1] + " * " + a[r_num2] + " + ";
+                                break;
+                            }
+                        case 2:
+                            {
+                                res += a[r_num1] - a[r_num2];
+                                rs += a[r_num1] + " - " + a[r_num2] + " + ";
+                                break;
+                            }
+                    }
+
+                    if (res == 15)
+                    { // Воала. Намерихме правилна формула
+                        Console.WriteLine(rs + " 0 = 15\n");
+                        found++;
+                        break;
+                    }
+
+                }
+                if (found > 5) break; // Не искаме повече от 5 формули
+            }
+        }
+    }
+}
