922a377595ad4e5f56d7a905bc77b4e0a0ebadb3
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-20 11:01:36 +0300

    Модул 4 Примери с Do While

---
+++ Modul4/ListExplore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дефиниции
+            string _userinput = "";
+
+            //List
+
+            //Потребителски вход
+            do
+            {
+                Console.Write("? ");
+                _userinput = Console.ReadLine();
+
+            } while (_userinput != "exit");
+
+            //Добавяне на стойности
+
+            //Преглед на List
+
+            //Размер на List
+
+
+        }
+    }
+}
