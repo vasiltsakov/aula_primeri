1455eabe65328adbcbca81439c68fe88bd077421
Vasil Tsakov <vasil.tsakov@gmail.com>
2017-07-22 17:07:11 +0300

    Модул 4 Задачи с цикли

---
+++ Modul4/Tasks_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 8)
+            {
+                i += 2;
+                Console.WriteLine(i.ToString());
+                if (i != 10)
+                {
+                    i--;
+                    Console.WriteLine(i.ToString());
+                }
+            }
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
---
+++ Modul4/Tasks_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> cw = Console.WriteLine;
+            List<string> z = new List<string>();
+
+            z.Add("ементал");
+            z.Add("дръжка");
+            z.Add("вафла");
+            z.Add("грозде");
+            z.Add("буркан");
+            z.Add("амфибия");
+
+            z.Sort();
+            cw(string.Join(", ", z));
+
+            z.Reverse();
+            cw(string.Join(", ", z));
+        }
+    }
+}
